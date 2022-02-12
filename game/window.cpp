fea9b5dc8613f4c5a5678b4689b30c59650e2dcf
Amine Ben Mbark <ambark@localhost.localdomain>
2022-02-12 12:50:32 +0100

    [ADD] folder game

---
+++ game/window.cpp
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2022
+** jam
+** File description:
+** window
+*/
+
+#include "window.hpp"
+
+int main()
+{
+    sf::Window window(sf::VideoMode(800, 600), "My window");
+
+    // on fait tourner le programme jusqu'à ce que la fenêtre soit fermée
+    while (window.isOpen())
+    {
+        // on inspecte tous les évènements de la fenêtre qui ont été émis depuis la précédente itération
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // évènement "fermeture demandée" : on ferme la fenêtre
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+    }
+
+    return 0;
+}
---
+++ game/window.hpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2022
+** jam
+** File description:
+** window
+*/
+
+#ifndef WINDOW_HPP_
+#define WINDOW_HPP_
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+
+#endif /* !WINDOW_HPP_ */
